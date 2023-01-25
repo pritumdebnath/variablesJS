4b7079d04efce51f01f442c0f93c220dd3b5fb8b
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-01-24 23:08:12 -0500

    new push

---
+++ app.js
@@ -0,0 +1,52 @@
+// 1
+var a = 0;
+var b = 1;
+var c = 2;
+var d = 3;
+var e = 4;
+var f = 5;
+var g = 6;
+var h = 7;
+var i = 8;
+var j = 9;
+
+// 2
+var sum = a + b;
+console.log(sum);
+
+var sum1 = a + c;
+console.log(sum1);
+
+// 3
+var sub = j - d;
+console.log(sub);
+
+var sub1 = i - d;
+console.log(sub1);
+
+// 4
+var multiply = e * f;
+console.log(multiply);
+
+var multiply1 = i * f;
+console.log(multiply1);
+
+// 5
+var divide = j / d;
+console.log(divide);
+
+var divide1 = g / c;
+console.log(divide1);
+
+// 6
+h++;
+
+i++;
+
+// 7
+d--;
+
+c--;
+
+// 8
+// Deployed to Github :D
